50712b7e7b82364d4b8e8f52ca606c925522c0c8
Julian Jolley <julian.jolley@mitel.com>
2021-07-02 21:31:36 +0100

    added bulma component to test

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,15 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<article class="message">
+		<div class="message-header">
+		  <p>Hello World</p>
+		  <button class="delete" aria-label="delete"></button>
+		</div>
+		<div class="message-body">
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+		</div>
+	  </article>
 </main>
 
 <style>
